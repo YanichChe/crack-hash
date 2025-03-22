ae5842fac050f0e14647953670e15a2cc97f3fa9
Chernovskaya <ychernovskaya@contorra.com>
2025-03-22 19:59:45 +0700

    Add rabbit-mq module

---
+++ rabbitmq/build.gradle.kts
@@ -0,0 +1,23 @@
+val rabbitmq_version: String by project
+
+plugins {
+    kotlin("jvm") version "2.1.10"
+    id("io.ktor.plugin") version "3.0.3"
+    id("org.jetbrains.kotlin.plugin.serialization") version "2.1.10"
+    id("application")
+}
+
+group = "ychernovskaya.crash.hash"
+version = "0.0.1"
+
+repositories {
+    mavenCentral()
+    maven { url = uri("https://packages.confluent.io/maven/") }
+}
+
+dependencies {
+    implementation("com.rabbitmq:amqp-client:$rabbitmq_version")
+    implementation("io.github.damirdenis-tudor:ktor-server-rabbitmq:1.3.3")
+    implementation("io.ktor:ktor-server-core")
+}
+
---
+++ rabbitmq/src/main/kotlin/ychernovskaya/crash/hash/RabbitMQSubscriber.kt
@@ -0,0 +1,45 @@
+package ychernovskaya.crash.hash
+
+import com.rabbitmq.client.AMQP
+import com.rabbitmq.client.DefaultConsumer
+import com.rabbitmq.client.Envelope
+
+data class SubscriberContext(
+    val queueName: String,
+    val consumerTag: String,
+    val routingKey: String
+)
+
+interface RabbitMQSubscriber {
+    suspend fun subscribe()
+}
+
+class RabbitMQSubscriberImpl(
+    private val subscriberContext: SubscriberContext,
+    private val connection: com.rabbitmq.client.Connection
+) : RabbitMQSubscriber {
+    var channel = connection.createChannel()
+
+    val autoAck = false;
+    override suspend fun subscribe() {
+        channel.basicConsume(subscriberContext.queueName, autoAck, subscriberContext.consumerTag,
+            object : DefaultConsumer(channel) {
+                override fun handleDelivery(
+                    consumerTag: String?,
+                    envelope: Envelope,
+                    properties: AMQP.BasicProperties?,
+                    body: ByteArray
+                ) {
+                    val routingKey = envelope.routingKey
+                    val deliveryTag = envelope.deliveryTag
+
+                    if (subscriberContext.routingKey == routingKey) {
+                        channel.basicAck(deliveryTag, false)
+                    } else {
+                        channel.basicNack(deliveryTag, false, true)
+                    }
+                }
+            }
+        )
+    }
+}
