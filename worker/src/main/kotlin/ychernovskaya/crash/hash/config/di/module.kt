f19926cdcf3bda859205f35ffba5a878358db04d
Chernovskaya <ychernovskaya@contorra.com>
2025-03-23 17:13:11 +0700

    Add worker subscriber

--- worker/src/main/kotlin/ychernovskaya/crash/hash/config/di/module.kt
+++ worker/src/main/kotlin/ychernovskaya/crash/hash/config/di/module.kt
@@ -1,16 +1,60 @@
 package ychernovskaya.crash.hash.config.di
 
+import com.fasterxml.jackson.dataformat.xml.XmlMapper
+import com.rabbitmq.client.Connection
 import org.koin.core.module.Module
 import org.koin.core.module.dsl.factoryOf
 import org.koin.dsl.bind
 import org.koin.dsl.module
+import ychernovskaya.crash.hash.RabbitMQConfiguration
+import ychernovskaya.crash.hash.RabbitMQConnection
+import ychernovskaya.crash.hash.loadRabbitMQConfiguration
+import ychernovskaya.crash.hash.pubsub.PublishContext
+import ychernovskaya.crash.hash.pubsub.RabbitMQPublisher
+import ychernovskaya.crash.hash.pubsub.RabbitMQPublisherImpl
+import ychernovskaya.crash.hash.pubsub.RabbitMQSubscriber
+import ychernovskaya.crash.hash.pubsub.RabbitMQSubscriberImpl
+import ychernovskaya.crash.hash.pubsub.SubscriberContext
+import ychernovskaya.crash.hash.services.ProcessMessage
 import ychernovskaya.crash.hash.services.WorkerService
 import ychernovskaya.crash.hash.services.WorkerServiceImpl
 
 fun appModule() = module {
     services()
+    queue()
+}
+
+private fun Module.queue() {
+    single {
+        PublishContext(
+            exchange = "crash-hash-exchange",
+            routingKey = "encoded-routing-key"
+        )
+    } bind PublishContext::class
+
+    single {
+        SubscriberContext(
+            queueName = "add-task-queue",
+            consumerTag = "worker",
+            routingKey = "add-task-routing-key"
+        )
+    } bind SubscriberContext::class
+
+    factoryOf(::RabbitMQPublisherImpl) bind RabbitMQPublisher::class
+    factoryOf(::RabbitMQSubscriberImpl) bind RabbitMQSubscriber::class
+
+    val config = loadRabbitMQConfiguration()
+    single { config } bind RabbitMQConfiguration::class
+
+    factoryOf(::RabbitMQPublisherImpl) bind RabbitMQPublisher::class
+    factoryOf(::RabbitMQSubscriberImpl) bind RabbitMQSubscriber::class
+
+    val rabbitMQConnection = RabbitMQConnection(config)
+    single { rabbitMQConnection.getConnection() } bind Connection::class
 }
 
 private fun Module.services() {
     factoryOf(::WorkerServiceImpl) bind WorkerService::class
+    single { XmlMapper() } bind XmlMapper::class
+    single { ProcessMessage(get(), get()) }
 }
--- worker/src/main/kotlin/ychernovskaya/crash/hash/config/setting/Frameworks.kt
+++ worker/src/main/kotlin/ychernovskaya/crash/hash/config/setting/Frameworks.kt
@@ -2,13 +2,21 @@ package ychernovskaya.crash.hash.config.setting
 
 import io.ktor.server.application.install
 import io.ktor.server.application.Application
+import io.ktor.server.application.ApplicationStarted
+import org.koin.ktor.ext.inject
 import org.koin.ktor.plugin.Koin
 import org.koin.logger.slf4jLogger
 import ychernovskaya.crash.hash.config.di.appModule
+import ychernovskaya.crash.hash.services.ProcessMessage
 
 fun Application.configureFrameworks() {
     install(Koin) {
         modules(appModule())
         slf4jLogger()
+    }
+
+    environment.monitor.subscribe(ApplicationStarted) {
+        val processMessage: ProcessMessage by inject()
+        processMessage.start()
     }
 }
