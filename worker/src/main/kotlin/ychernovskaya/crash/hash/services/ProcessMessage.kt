143ff23d63011a8c376edb6eb345a886609cb233
Chernovskaya <ychernovskaya@contorra.com>
2025-03-30 15:52:59 +0700

    Update status

--- worker/src/main/kotlin/ychernovskaya/crash/hash/services/ProcessMessage.kt
+++ worker/src/main/kotlin/ychernovskaya/crash/hash/services/ProcessMessage.kt
@@ -5,6 +5,8 @@ import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import org.slf4j.LoggerFactory
+import ychernovskaya.crack.hash.storage.ProcessInfoStorage
+import ychernovskaya.crack.hash.storage.Status
 import ychernovskaya.crash.hash.HashData
 import ychernovskaya.crash.hash.model.CrackHashManagerRequest
 import ychernovskaya.crash.hash.pubsub.RabbitMQSubscriber
@@ -12,7 +14,8 @@ import ychernovskaya.crash.hash.pubsub.RabbitMQSubscriber
 class ProcessMessage(
     private val rabbitMQSubscriber: RabbitMQSubscriber,
     private val xmlMapper: XmlMapper,
-    private val workerService: WorkerService
+    private val workerService: WorkerService,
+    private val processInfoStorage: ProcessInfoStorage
 
 ) {
     val logger = LoggerFactory.getLogger(ProcessMessage::class.java)
@@ -22,6 +25,12 @@ class ProcessMessage(
             rabbitMQSubscriber.subscribe { message ->
                 val request = xmlMapper.readValue(message, CrackHashManagerRequest::class.java)
                 logger.info("Get new task")
+
+                processInfoStorage.updateStatusByRequestIdAndPartNumber(
+                    requestId = request.requestId,
+                    partNumber = request.partNumber,
+                    status = Status.Pending
+                )
 
                 workerService.addEncodeHashTask(
                     hashData = HashData(
